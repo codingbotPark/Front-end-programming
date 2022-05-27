935d61941963d2dcbdfb911083541d3dd74ac01f
박병관 <85085375+codingbotPark@users.noreply.github.com>
2022-05-27 09:32:48 +0900

    move birthday

--- birthday/rkdus05/src/main/main.js
+++
@@ -1,8 +0,0 @@
-const imgT = document.getElementById("firstImg");
-const comment = document.getElementById("comment");
-
-imgT.addEventListener("click",function(){
-    // console.log("Gd")
-    imgT.src = '../assets/나.png'
-    comment.innerHTML = "선물은 나다!"
-})
