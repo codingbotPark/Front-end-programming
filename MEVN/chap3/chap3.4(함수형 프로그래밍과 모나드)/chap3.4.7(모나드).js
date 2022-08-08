88384050d68890a68cec75dbcba4c3ec7136df2a
박병관 <85085375+codingbotPark@users.noreply.github.com>
2022-08-09 00:24:03 +0900

    study 모나드2

--- MEVN/chap3/chap3.4(함수형 프로그래밍과 모나드)/chap3.4.7(모나드).js
+++ MEVN/chap3/chap3.4(함수형 프로그래밍과 모나드)/chap3.4.7(모나드).js
@@ -12,5 +12,12 @@ fg('{"temp":36.5}').catch(_=> 'JSON PARSE is not working').then(log)
 
 
 // 퓨처모나드(Promise)2
+// Promise.reject를 반환하여 catch문에 들어간다(위는 그냥 오류)
 const user = [{name:'큰돌'}, {name:'병관'},{name:'김개발'},{name:'다빈'}]
- 
+const g1 = (name) => user.find(u => u.name === name) || Promise.reject("객체에 없습니다")
+
+const f1 =  ({name}) => `${name}이가 춤을 춥니다`
+const fg1 = x => new Promise((resolve,reject) => resolve(x)).then(g1).then(f1)
+
+fg1("큰돌1").catch(_=>_).then(console.log)
+fg1("큰돌").catch(_=>_).then(console.log)
