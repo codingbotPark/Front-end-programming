591fe76704913784af2d5cbe0e1a011429f0c675
박병관 <85085375+codingbotPark@users.noreply.github.com>
2022-08-17 11:56:44 +0900

    make img_slider

--- MEVN/chap5/chap5.4(vue.js로 이미지 슬라이더 만들기)/server/server.js
+++ MEVN/chap5/chap5.4(vue.js로 이미지 슬라이더 만들기)/server/server.js
@@ -2,11 +2,11 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-const img_path = path.join(__dirname,`./img`)
-const PORT = 12011
+const img_path = path.join(__dirname,'./img')
+const PORT = 12010
 
 const main = async () => {
-    app.use('./img',express.static(img_path))
+    app.use('/img',express.static(img_path))
     app.listen(PORT,console.log('이미지 서버 시작'))
 }
 
