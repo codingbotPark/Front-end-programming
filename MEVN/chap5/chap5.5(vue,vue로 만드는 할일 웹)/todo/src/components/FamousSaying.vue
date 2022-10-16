546b9c91f888a06e65cce4eb797a805ccd8141e2
박병관 <85085375+codingbotPark@users.noreply.github.com>
2022-10-16 18:07:00 +0900

    try todo with todo

---
+++ MEVN/chap5/chap5.5(vue,vue로 만드는 할일 웹)/todo/src/components/FamousSaying.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="sayingContainer">
+        <h1></h1>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        name:'FamousSaying',
+    }
+</script>
+
+
+<style>
+
+</style>
